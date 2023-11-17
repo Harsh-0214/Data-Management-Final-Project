21e6b5c16a29b8615a95ec29b1fe85e563729789
Harsh Tamakuwala <harsh_2003@hotmail.com>
2023-11-16 22:19:59 -0500

    created css file, and webpages
    css file must be linked to the program, idk why its not working

--- routes.js
+++ routes.js
@@ -3,9 +3,16 @@ var express = require("express");
 
 var router = express.Router();
 
+var fs = require('fs');
+var myCss = {
+    style : fs.readFileSync('css/style.css','utf8')
+};
+
 router.get("/", function (req,res){
     console.log("hello im on start page");
-    res.render("index");
+    res.render("index.ejs");
+    title: 'AODB'
+    myCss: myCss;
 
 });
 
