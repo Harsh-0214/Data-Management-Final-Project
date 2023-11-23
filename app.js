8d9533b9aa62ab81de557c53b6f649be92530a64
Hasan Khan <115020762+HasanK404@users.noreply.github.com>
2023-11-22 19:38:00 -0500

    All pages complete with correct javascript and routing
    post method is required before submission.

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 var express = require("express");
 var path =require("path");
 
-
-var routes = require("./routes"); //linking routes file
-
 //setting up port
 var app =express();
 app.set("port", process.env.PORT || 3000);
